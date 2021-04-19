0cce3f8e90045a22b0b0711376a13a6e49400779
= <kaiwalyakoparkar@gmail.com>
2021-04-19 16:35:20 +0530

    Learned about the templating in js

--- Vanilla-Js-Playlist/scripts/main.js
+++ Vanilla-Js-Playlist/scripts/main.js
@@ -9,6 +9,9 @@ const inputDescEl = document.querySelector("#inputDesc");
 //ref to input amount
 const inputElement = document.querySelector("#inputAmount");
 
+//getting the ref to the table
+const expenseTableEl = document.querySelector("#expenseTable");
+
 //Set the heading element to totalExpense
 headingEl.textContent = totalExpense;
 
@@ -32,15 +35,27 @@ function AddExpenseToTotal(){
     expenseItem.amount = expense;
 
     allExpenses.push(expenseItem);
-
-    console.clear();
-    console.table(allExpenses);
     
     //add current expense to total expense
     totalExpense = totalExpense + expense; 
 
     //Set heading element to totalExpense
-    headingEl.textContent = totalExpense;
+    const someText = `Total is: ${totalExpense}`;
+    headingEl.textContent = someText;
+
+    // Show the table here
+    const data1 = allExpenses[0];
+    const data2 = allExpenses[1];
+
+    const data1Text = `${data1.amount}:: ${data1.desc}`;
+    const data2Text = `${data2.amount}:: ${data2.desc}`
+
+    const tableText = `
+        <div>${data1Text}</div>
+        <div>${data2Text}</div>
+    `
+    expenseTableEl.innerHTML = tableText;
+
 }
 
 //Get the button element
