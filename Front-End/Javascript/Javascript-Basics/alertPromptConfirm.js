e00da6faab568e8ba92c32f8ef11edeb1e8c3849
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-05-18 19:05:40 +0530

    feat: :rocket: Confirm, Prompt, alert added

---
+++ Front-End/Javascript/Javascript-Basics/alertPromptConfirm.js
@@ -0,0 +1,11 @@
+//This just gives a popup with the entered message
+alert("Wait a minute who are you?");
+
+//This checks the response from the user and acts accordingly. It will give user two option "okay" or "cancel"
+let userResponse = confirm("Do you agree with company policies?");
+//Now you can do whatever you want on the basis of the user reaction
+console.log(userResponse);
+
+//This gives user a field to type the answer and then you can use the response collected for you own tasks
+let userReply = prompt("How should we improve?");
+console.log(userReply);
