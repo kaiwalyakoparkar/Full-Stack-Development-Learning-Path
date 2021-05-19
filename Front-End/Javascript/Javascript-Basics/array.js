61213480a7016911421017821a67f72843602d6f
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-05-19 17:55:37 +0530

    feat: :fire: Learned about the array and manipulated them

---
+++ Front-End/Javascript/Javascript-Basics/array.js
@@ -0,0 +1,5 @@
+let array = ["hello ","I am", " Kaiwalya"];
+console.log(array[0] + array[1] + array[2]);
+
+array[1] = "I was";//Manipulated the elements on the 1st index which is "I am" with "I was"
+console.log(array[0] + array[1] + array[2]);
