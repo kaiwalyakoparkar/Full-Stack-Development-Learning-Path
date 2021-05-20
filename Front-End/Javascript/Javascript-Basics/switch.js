94fe75391b9b936918358130263e49a26663fe41
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-05-20 17:10:39 +0530

    Add: :rocket: Added the switch statement

---
+++ Front-End/Javascript/Javascript-Basics/switch.js
@@ -0,0 +1,19 @@
+let letter = 'a';
+
+switch (letter){
+    case 'A':
+    case 'a':
+    case 'E':
+    case 'e':
+    case 'I':
+    case 'i':
+    case 'O':
+    case 'o':
+    case 'U':
+    case 'u':
+        console.log("It is a Vowel");
+    break;
+    default:
+        console.log("It is a Consonent");
+    break;
+}
