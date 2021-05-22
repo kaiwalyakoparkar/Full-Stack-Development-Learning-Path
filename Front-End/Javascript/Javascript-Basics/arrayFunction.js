515127bb5a63ea0b8f80278788f7ab08f9aa5dc6
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-05-22 09:25:24 +0530

    Add: :fire: Array.filter() function added

--- Front-End/Javascript/Javascript-Basics/arrayFunction.js
+++ Front-End/Javascript/Javascript-Basics/arrayFunction.js
@@ -1,7 +1,18 @@
 //Array.map() function for traversing the array and perform certain tasks
-let array1 = [1,2,3,4,5];
-let array2 = array1.map((element)=>{
+let arrayMap1 = [1,2,3,4,5];
+let arrayMap2 = arrayMap1.map((element)=>{
     return element*2;
 });
-console.log(array2);
+//Will print [ 2, 4, 6, 8, 10 ]
+console.log(arrayMap2);
 
+//Array.filter() function returns the element only if the condition is true
+let arrayFilter1 = [1,2,3,4,5];
+let arrayFilter2 = arrayFilter1.filter((element) => {
+    if(element > 2){
+        return true;
+    }
+    return false;
+});
+//Will print only [3,4,5] coz 1 & 2 are lesser than 2;
+console.log(arrayFilter2);
