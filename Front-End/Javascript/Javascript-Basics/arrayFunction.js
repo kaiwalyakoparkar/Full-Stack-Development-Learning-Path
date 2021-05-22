f2c8d13122d8348645625d9cf56c95497412ba5d
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-05-22 09:10:03 +0530

    Add: :rocket: Added .map() function

---
+++ Front-End/Javascript/Javascript-Basics/arrayFunction.js
@@ -0,0 +1,7 @@
+//Array.map() function for traversing the array and perform certain tasks
+let array1 = [1,2,3,4,5];
+let array2 = array1.map((element)=>{
+    return element*2;
+});
+console.log(array2);
+
