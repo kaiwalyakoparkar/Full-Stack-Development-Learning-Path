111aa67a595a36daec8dd41875f3cec47b6b11ef
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-05-26 09:28:20 +0530

    Add: :fire: Learned removing the element from the document and removing and reassinging the attribute to the element

--- Front-End/Javascript-Intermidiate/CRUD.js
+++ Front-End/Javascript-Intermidiate/CRUD.js
@@ -19,13 +19,29 @@ let text = document.createTextNode('Hello this is the text node representation')
 elem2.appendChild(text);
 elem2.className = 'custom-heading';
 elem2.id = 'heading-three'
-console.log(elem2);
+// console.log(elem2);
 
 //Replacing the custom created element
 element.replaceWith(elem2);//replacing the contents on element position by elem2 (This doesn't change the attributes of the element anywhere)
 
 //Replacing the element with id 'change-here'
 let parent = document.querySelector('.container');//Selecting the parent class whose children needs to be replced
-console.log(parent);
-//Parent ----------> newContent, oldContent(selected by id)
+// console.log(parent);
+//Parent ---------> newContent, oldContent(selected by id)
 parent.replaceChild(element, document.querySelector('#change-here'));
+
+
+//----------- This is removing the element
+let parent2 = document.querySelector('.container');
+//This will remove the element with id remove-this from the parent2.
+parent2.removeChild(document.querySelector('#remove-this'));
+
+//----------- This is for setting and removing the 'attributes' of the 'element'
+let elem3 = document.createElement('h5');
+elem3.className = 'attribute-remove';
+elem3.id = 'attribute-remove';
+console.log(elem3);
+
+elem3.removeAttribute('id');//Removing the id attribute
+elem3.setAttribute('id', 'hover');//Reassigning the id attribute
+console.log(elem3.getAttribute('id'));
