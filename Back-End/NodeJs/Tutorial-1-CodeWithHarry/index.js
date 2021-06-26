3a5bf6cf568815f3f6e740dad426dd551a898447
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-06-26 13:58:40 +0530

    :truck: OS imported and the functions used

--- Back-End/NodeJs/Tutorial-1-CodeWithHarry/index.js
+++ Back-End/NodeJs/Tutorial-1-CodeWithHarry/index.js
@@ -1,3 +1,12 @@
 const onlyUser = require('./second');
 
 console.log("Hello ", onlyUser.name);
+
+//Using OS
+const os = require('os');
+
+console.log(os.homedir());//Logs the home directory location
+console.log(os.totalmem());//Logs the total memory present in the computer.
+console.log(os.freemem());//Logs the free memory available in the computer
+console.log(os.type());//Logs the OS present on the computer
+console.log(os.uptime());//Logs the total time computer is on.
