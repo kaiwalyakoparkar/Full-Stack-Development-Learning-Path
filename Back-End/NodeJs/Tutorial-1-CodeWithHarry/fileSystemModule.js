4bc5be7b9462c1e3f02092ceae47b822ee98e71b
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-06-26 15:09:01 +0530

    :boom: Wrote the file using writeFile and writeFileSync functions of fileSystemModulee

--- Back-End/NodeJs/Tutorial-1-CodeWithHarry/fileSystemModule.js
+++ Back-End/NodeJs/Tutorial-1-CodeWithHarry/fileSystemModule.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+/*
+Theory: 
+if you execture the function with Sync at the end then nodejs will stop until the function is completely executed. If Sync is not used then the next commands and operations will take place before the function.
+*/
 fs.readFile('fileSystemReadFile.txt', 'utf8', (error, data) => {
     console.log(error, data);
 });//This will read the given file.
@@ -9,3 +13,11 @@ console.log('Successfully Read file with fs.readfile()');//This appears before t
 const syncFile = fs.readFileSync('fileSystemReadFile.txt');
 console.log(syncFile.toString());//This will convert the buffer string to readable string
 console.log('Successfully Read file with fs.readfileSync()');//Now this will be executed after the readFileSync function only as the node js stops till the exectution of the function is done and is ready to proceed ahead.
+
+fs.writeFile('fileSystemWriteHere.txt','Did this got written', ()=>{
+    console.log("Successfully wrote it!!");
+});
+
+
+const b = fs.writeFileSync('fileSystemWriteHere.txt', 'Did this got written with sync as well?');//This will overwritte everything present in the file.
+console.log('Successfully wrote it!!');
