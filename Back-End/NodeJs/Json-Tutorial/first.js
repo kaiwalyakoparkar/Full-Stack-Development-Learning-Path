8e4eefbfcc417862a919ef1f592b1a02683f7334
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-06-29 17:52:31 +0530

    :heavy_plus_sign: JSON leaned and included in js file

--- Back-End/NodeJs/Json-Tutorial/first.js
+++ Back-End/NodeJs/Json-Tutorial/first.js
@@ -1,15 +1,12 @@
-let sample = {
-    name: "Kaiwalya",
-    score: 11.75,
-    isAdmin: true,
-    programmingLanguages:["java","javascript","c++", {
-        typescript: true,
-        ruby: false
-    }],
-    myObj:{
-        name:"nested",
-        marks: 52
-    }
-}
+let data = require('./student.json');//Includes the Json file from the local environment
 
-console.log(sample.programmingLanguages[3].typescript);
+/*
+We can use fetch here (which is used for api) but in Node js require is preferred and emphasised more. fetch is solely used for api integration.
+*/
+
+console.log('All students: ');
+
+//Iterating over all the student names
+for(let i = 0; i < 2; i++){
+    console.log(data.students[i].name);
+}
