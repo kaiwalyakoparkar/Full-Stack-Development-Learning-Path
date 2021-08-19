8ce7b5cc86c500e9640efdf4f2b792c5072ff1c1
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-08-19 09:37:21 +0530

    feat: :rocket: added db connection and installed mongoose

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/server.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/server.js
@@ -1,10 +1,9 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./index.js');
 const port = process.env.PORT || 3000;
-
-// console.log(process.env);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
