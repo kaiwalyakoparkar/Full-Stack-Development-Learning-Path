6e847a3b046c65255488b10a41201aeaa5018704
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-08-19 09:38:11 +0530

    feat: :rocket: Mongoose connection added

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/server.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/server.js
@@ -5,6 +5,17 @@ dotenv.config({ path: './config.env' });
 const app = require('./index.js');
 const port = process.env.PORT || 3000;
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
