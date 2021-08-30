c544ecd50f81e072005e0224af1f86dca7868f91
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-08-30 10:26:26 +0530

    feat: Advance modeling data and running script

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/tourModel.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/tourModel.js
@@ -7,14 +7,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Tour should have a name'],
     unique: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour should have a difficulty']
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'Tour should have a price']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'Tour should have a Summary'],
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 //Creating model out of the tours schema
