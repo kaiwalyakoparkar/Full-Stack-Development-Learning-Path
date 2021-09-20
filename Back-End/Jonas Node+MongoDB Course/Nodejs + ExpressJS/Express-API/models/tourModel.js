1fc19196440beaf525ac8d1562c7c776c6758850
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-20 19:25:18 +0530

    feat: Document middlewares (pre hooks and post hooks) learnt

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/tourModel.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/tourModel.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 //Created a tours schema
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Tour should have a name'],
+    unique: true
+  },
+  slug: {
+    type: String,
+    require: [true, 'Tour must have a slug'],
     unique: true
   },
   duration: {
@@ -57,9 +63,27 @@ const tourSchema = new mongoose.Schema({
   toObject: {virtuals: true}
 });
 
+//VIRTUAL PROPERTIES
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 })
+
+//DOCUMENT MIDDLEWARE runs before .save() and .create()
+//Pre-Save-hook / pre middleware 
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true});
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Saving the document... ⏱');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 //Creating model out of the tours schema
 const Tour = mongoose.model('Tour', tourSchema);
