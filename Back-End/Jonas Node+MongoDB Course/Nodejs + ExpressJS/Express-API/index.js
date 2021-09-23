28a6a0dcf03378ffd19e8f03d405a5fee97e65a8
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-23 21:40:17 +0530

    feat: Err Handling:: Added error class file for handling errors

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/index.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/index.js
@@ -6,6 +6,8 @@ const app = express();
 const tourRoute = require(path.join(__dirname, './routes/tourRoutes.js'));
 const userRoute = require(path.join(__dirname, './routes/userRoutes.js'));
 
+const appError = require(path.join(__dirname, './utils/appError.js'));
+
 //My custom middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -31,22 +33,30 @@ app.use('/api/v1/users', userRoute);
 //error handling if no route is catched (This block of code should be always at last of route handling else it will throw this error for each and every request we make)
 app.all('*', (req, res, next) => {
 
-  //Earlier error handler
+  //Earlier error handler (Iteration 1)
   // res.status(404).json({
   //   status: 'fail',
   //   message: `${req.originalUrl} was not found on the server. Please check the Url :)`
   // });
 
-  //Passing the error to new global error handler
-  const err = new Error(`${req.originalUrl} was not found on the server. Please check the Url :)`);
-  err.statusCode = 404;
-  err.status = 'fail';
+  //Passing the error to new global error handler (Iteration 2)
+  // const err = new Error(`${req.originalUrl} was not found on the server. Please check the Url :)`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
   //whenever something is passed in next in any middleware then it is always considered as error and then sent to global error handling middleware.
-  next(err);
+  // next(err);
+
+  //Iteration 3 (final iteration)
+  //Using the newly created appError class to reduce redundant creation of error handling code
+  next(new appError(`${req.originalUrl} was not found on the server. Please check the Url :)`));
 });
 
-//error handling middleware
+//Global Error handling middleware
 app.use((err, req, res, next) => {
+
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'err';
 
---
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/utils/appError.js
@@ -0,0 +1,12 @@
+class appError extends Error {
+	constructor(message, statusCode) {
+		super(message);
+		this.statusCode = statusCode;
+		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+		this.isOperational = true;
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+};
+
+module.exports = appError;
