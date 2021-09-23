29fb5b5208e4d574664281a2d15ed4182ddf55e5
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-23 21:13:17 +0530

    feat: Err Handling:: Implemented global error handling middleware

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/index.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/index.js
@@ -25,11 +25,34 @@ app.use(express.static(path.join(__dirname+'/public')));
 app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
 
-//Error handling if no route is catched (This block of code should be always at last of route handling else it will throw this error for each and every request we make)
+
+// ================== ERROR HANDLING ===============
+
+//error handling if no route is catched (This block of code should be always at last of route handling else it will throw this error for each and every request we make)
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `${req.originalUrl} was not found on the server. Please check the Url :)`
+
+  //Earlier error handler
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `${req.originalUrl} was not found on the server. Please check the Url :)`
+  // });
+
+  //Passing the error to new global error handler
+  const err = new Error(`${req.originalUrl} was not found on the server. Please check the Url :)`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  //whenever something is passed in next in any middleware then it is always considered as error and then sent to global error handling middleware.
+  next(err);
+});
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'err';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
   next();
 });
