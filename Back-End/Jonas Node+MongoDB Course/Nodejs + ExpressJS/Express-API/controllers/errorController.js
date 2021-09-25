b855da96399fd0bba6a454d1eb91312aab740367
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-25 17:51:33 +0530

    feat: Err Handling:: Seperated the erros in development environment and production environment

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/controllers/errorController.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/controllers/errorController.js
@@ -1,3 +1,34 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err
+  });
+}
+
+const sendErrorProd = (err, res) => {
+  //If operational then send the alloted error message
+  if(err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+  //If not operational then don't leak the error message instead send a generic error message
+  } else {
+    //1) Log the error in the console so that you can get a report of the error
+    console.error('Error 💥 :',err);
+
+    //2) Send generic message to the user.
+    res.status(500).json({
+      status: 'error',
+      message: 'This error seems to be non-operational. You can join the support channels on discord to ask your questions'
+    })
+  }
+  
+}
+
 module.exports = (err, req, res, next) => {
 
   // console.log(err.stack);
@@ -5,9 +36,10 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'err';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-  next();
+  if(process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if(process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+  
 }
