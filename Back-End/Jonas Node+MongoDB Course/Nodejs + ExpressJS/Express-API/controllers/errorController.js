38a2f4683c4cb52fb980d164fc35857db4309714
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-25 20:38:57 +0530

    feat: Err Handling:: Handling invalid database ids

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/controllers/errorController.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require('../utils/appError.js');
+require('dotenv').config();
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,7 +47,12 @@ module.exports = (err, req, res, next) => {
   if(process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if(process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = {...err};
+
+    if(error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    sendErrorProd(error, res);
   }
   
 }
