4e3db87a4589ef5e5b644a8c5bc0570b010926cd
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-28 20:58:28 +0530

    feat: Fixed a possible vurnability while signing up as a new user

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/controllers/authController.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel.js');
 const catchAsync = require('../utils/catchAsync.js');
 
 exports.signup = catchAsync(async(req, res, next) => {
-	const newUser = await User.create(req.body);
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passConfirm: req.body.passConfirm
+	});
 
 	res.status(201).json({
 		status: 'success',
