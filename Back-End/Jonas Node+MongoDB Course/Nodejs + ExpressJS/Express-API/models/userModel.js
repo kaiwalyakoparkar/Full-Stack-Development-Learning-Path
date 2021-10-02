2e2d9f0c86744eb2eda47eb10fad9ddfdaf8277e
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-10-02 21:18:48 +0530

    feat: Removed unwanted console logs

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/userModel.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/userModel.js
@@ -79,7 +79,6 @@ userSchema.methods.createPasswordResetToken = function() {
 	this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
 	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
-	console.log({resetToken}, this.passwordResetToken);
 	return resetToken;
 }
 
