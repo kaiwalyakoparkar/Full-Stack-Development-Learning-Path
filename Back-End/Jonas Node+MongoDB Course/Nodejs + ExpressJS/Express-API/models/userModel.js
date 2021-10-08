29de616f786f1238942f370fdaa0691b9ec7f419
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-10-08 19:48:45 +0530

    feat: Implemented model to set active state of the user

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/userModel.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/models/userModel.js
@@ -42,7 +42,12 @@ const userSchema = new mongoose.Schema({
 	},
 	passwordChangedAt: Date,
 	passwordResetToken: String,
-	passwordResetExpires: Date
+	passwordResetExpires: Date,
+	active: {
+		type: Boolean,
+		default: true,
+		select: false
+	}
 });
 
 //MIDDLEWARES
