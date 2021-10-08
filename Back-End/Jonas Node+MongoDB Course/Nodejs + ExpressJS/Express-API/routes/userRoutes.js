536f196ec09f310e8a7cf5a1c0803201f8fba8b0
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-10-08 20:10:23 +0530

    feat: deleteMe route created

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/routes/userRoutes.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/routes/userRoutes.js
@@ -11,6 +11,7 @@ route.patch('/resetPassword/:token', authController.resetPassword);
 
 route.patch('/updatePassword', authController.protect,authController.updatePassword);
 route.patch('/updateMe', authController.protect,userController.updateMe);
+route.delete('/deleteMe', authController.protect,userController.deleteMe);
 
 route
   .route('/')
