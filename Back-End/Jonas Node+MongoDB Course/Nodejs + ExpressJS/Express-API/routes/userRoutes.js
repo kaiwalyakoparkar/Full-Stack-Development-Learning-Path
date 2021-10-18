28e19736157de5cb29e885b3df6c10953e085f75
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-10-18 19:14:09 +0530

    feat: Resize middleware implemented in a route

--- Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/routes/userRoutes.js
+++ Back-End/Jonas Node+MongoDB Course/Nodejs + ExpressJS/Express-API/routes/userRoutes.js
@@ -11,7 +11,7 @@ route.post('/forgotPassword', authController.forgotPassword);
 route.patch('/resetPassword/:token', authController.resetPassword);
 
 route.patch('/updatePassword', authController.protect,authController.updatePassword);
-route.patch('/updateMe', authController.protect, userController.uploadUserPhoto, userController.updateMe);
+route.patch('/updateMe', authController.protect, userController.uploadUserPhoto, userController.resizeUserPhote,userController.updateMe);
 route.delete('/deleteMe', authController.protect,userController.deleteMe);
 route.get('/me', authController.protect, userController.getMe, userController.getSingleUser);
 
