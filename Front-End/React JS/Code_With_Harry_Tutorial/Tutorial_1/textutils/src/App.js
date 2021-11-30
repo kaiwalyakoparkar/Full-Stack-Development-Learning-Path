0234b77c8b02c8a84c71d56ced0480fa8fbc0170
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-11-30 18:42:47 +0530

    feat: Started 1st project and added navbar | Learned JSX

--- Front-End/React JS/Code_With_Harry_Tutorial/Tutorial_1/textutils/src/App.js
+++ Front-End/React JS/Code_With_Harry_Tutorial/Tutorial_1/textutils/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Text Utils</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
