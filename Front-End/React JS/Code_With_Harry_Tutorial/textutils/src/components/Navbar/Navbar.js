ecc91e84e6b7dafd705ff355054a4e28715f7996
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-02 20:21:07 +0530

    feat: Added clear text and reverse text functionalities to the application

--- Front-End/React JS/Code_With_Harry_Tutorial/textutils/src/components/Navbar/Navbar.js
+++ Front-End/React JS/Code_With_Harry_Tutorial/textutils/src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import propTypes from 'prop-types'
 export default function Navbar(props) {
 	return(
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 		        <div className="container-fluid">
 		          <a className="navbar-brand" href="/">{props.title}</a>
 		          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- Front-End/React JS/Code_With_Harry_Tutorial/textutils/src/components/TextForm/TextForm.js
+++ Front-End/React JS/Code_With_Harry_Tutorial/textutils/src/components/TextForm/TextForm.js
@@ -13,19 +13,47 @@ export default function TextForm(props) {
 		setText(textToUppercase);
 	}
 
+	const handleOnLowClick = () => {
+		const textToLowercase = text.toLowerCase();
+		setText(textToLowercase);
+	}
+
+	const handleClearText = () => {
+		setText('');
+	}
+
+	const handleReverseClick = () => {
+		const reversedText = text.split("").reverse().join("");
+		setText(reversedText);
+	}
+
 	//Using states text is original state and we can change it via setText method.
-	const [text, setText] = useState('Enter your text here');
+	const [text, setText] = useState('');
 
 	// text = 'This is wrong way of setting the state. This will generate errors'
 	// setText('This is the correct way of setting the state');
 
 	return (
-		<div>
-			<div className="mb-3">
-				  <h1>{props.heading}</h1>
-				  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+		<div className="conainer">
+			<div className="container">
+				<div className="mb-3">
+					  <h1>{props.heading}</h1>
+					  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+				</div>
+				<div className="container">
+					<button className="btn btn-primary mx-3 my-2" onClick={handleOnUpClick}>Convert to Upper Case</button>
+					<button className="btn btn-primary mx-3 my-2" onClick={handleOnLowClick}>Convert to Lower Case</button>
+					<button className="btn btn-primary mx-3 my-2" onClick={handleClearText}>Clear text</button>
+					<button className="btn btn-primary mx-3 my-2" onClick={handleReverseClick}>Reverse text</button>
+				</div>
 			</div>
-			<button className="btn btn-primary" onClick={handleOnUpClick}>Convert to Upper Case</button>
+			<div className="container my-3">
+				<h1>Your text summary is:</h1>
+				<p>{text.split(" ").length} words and {text.length} characters</p>
+				<p>{0.008 * text.split(" ").length} minutes of read time</p>
+				<h2>Preview:</h2>
+				<p>{text}</p>
+			</div>
 		</div>
 	)
 }
