3e85035833b01fb45999895847558b00245fe2df
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-15 20:21:17 +0530

    fix: :hammer: Fixed the image dimentions and ensured best practices

--- Front-End/React JS/Practice-Projects/portfolio2/src/components/Home/Home.js
+++ Front-End/React JS/Practice-Projects/portfolio2/src/components/Home/Home.js
@@ -4,7 +4,7 @@ import programmerImage from '../../assets/images/programmer.jpg'
 export default function Home() {
 	return (
 		<div className="homeContainer">
-			<img className="profile-image" src={programmerImage}/>
+			<img className="profile-image" alt="Programmer profile" src={programmerImage}/>
 			<div className="textArea">
 				<h1 className="titleName">Programmer</h1>
 				<p className="info-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
