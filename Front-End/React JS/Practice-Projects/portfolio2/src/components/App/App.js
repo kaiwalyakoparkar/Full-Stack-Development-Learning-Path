f685944a5c12b6cbcf99bb4ce32965a18d0f9f7b
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-15 21:53:23 +0530

    feat: Implementing the timeline from prime react js

--- Front-End/React JS/Practice-Projects/portfolio2/src/components/App/App.js
+++ Front-End/React JS/Practice-Projects/portfolio2/src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from '../Navbar/Navbar.js';
-import Home from '../Home/Home.js';
+// import Home from '../Home/Home.js';
+import About from '../About/About.js';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      {/*<Home />*/}
+      <About />
     </div>
   );
 }
