0564997e4f9239507cc27acdf041cf439797985e
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-30 19:35:31 +0530

    fix: :hammer: Fixed the react router to make portfolio multipage

--- Front-End/React JS/Practice-Projects/portfolio2/src/components/App/App.js
+++ Front-End/React JS/Practice-Projects/portfolio2/src/components/App/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Navbar from '../Navbar/Navbar.js';
-// import Home from '../Home/Home.js';
-// import About from '../About/About.js';
+import Home from '../Home/Home.js';
+import About from '../About/About.js';
 import Contact from '../Contact/Contact.js';
+
+import { BrowserRouter as Router, Routes as Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <Navbar />
-      {/*<Home />*/}
-      {/*<About />*/}
-      <Contact />
+      <Router>  
+        <Navbar />
+        <Switch>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- Front-End/React JS/Practice-Projects/portfolio2/src/components/Navbar/Navbar.js
+++ Front-End/React JS/Practice-Projects/portfolio2/src/components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
 import './Navbar.css';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function Navbar () {
 	return (
 		<div>
 			<nav className="navbar navbar-expand-lg navbar-dark bg-info">
-			  <a className="navbar-brand" href="/">Portfolio 2</a>
+			  <Link className="navbar-brand" to="/">Portfolio 2</Link>
 			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			    <span className="navbar-toggler-icon"></span>
 			  </button>
 			  <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
 			    <div className="navbar-nav">
-			      <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
-			      <a className="nav-item nav-link" href="/">About</a>
-			      <a className="nav-item nav-link" href="/">Contact</a>
+			      <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
+			      <Link className="nav-item nav-link" to="/about">About</Link>
+			      <Link className="nav-item nav-link" to="/contact">Contact</Link>
 			    </div>
 			  </div>
 			</nav>
