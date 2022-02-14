2b70c9cc9555463b109bb0d8188b834982caeb34
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-02-14 19:24:41 +0530

    refactor: :art: Removed unnecessary imports

--- Front-End/Material UI/The Net Ninja Tutorial/notes-app/src/index.js
+++ Front-End/Material UI/The Net Ninja Tutorial/notes-app/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import App from './components/App/App.js';
 
 ReactDOM.render(
