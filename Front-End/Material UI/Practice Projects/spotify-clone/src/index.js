534015ef9668f54b34019cd349a9a015783ca41a
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-02-24 20:28:36 +0530

    feat: :rocket: Component & font family added

--- Front-End/Material UI/Practice Projects/spotify-clone/src/index.js
+++ Front-End/Material UI/Practice Projects/spotify-clone/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
   document.getElementById('root')
 );
