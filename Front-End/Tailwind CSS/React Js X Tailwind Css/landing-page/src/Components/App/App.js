493e6bf7c8c908cd8c4f736dc033f29d2f68af30
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-04-30 20:55:09 +0530

    feat: :rocket: Left pannel completed

--- Front-End/Tailwind CSS/React Js X Tailwind Css/landing-page/src/Components/App/App.js
+++ Front-End/Tailwind CSS/React Js X Tailwind Css/landing-page/src/Components/App/App.js
@@ -1,10 +1,11 @@
+import Landing from "../Landing/Landing";
 import Navbar from "../Navbar/Navbar";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-4xl">Hello</h1>
+      <Landing />
     </div>
   )
 }
---
+++ Front-End/Tailwind CSS/React Js X Tailwind Css/landing-page/src/Components/Landing/Landing.js
@@ -0,0 +1,32 @@
+export default function Landing() {
+    return (
+        <div className="flex bg-indigo-900 text-white h-screen rounded-lg p-8">
+
+            <div className="flex-row w-3/4 m-4">
+
+                <div className="text-6xl mb-8">
+                    Live chat support for your customers.
+                </div>
+
+                <div className="text-xl mb-8">
+                    Create custom landing pages with CompanyZ that convert <br /> more visitors than any website—no coding required.
+                </div>
+
+                <div className="flex mb-8">
+                    <input type="email" placeholder="you@example.com" className="mr-8 mt-1 px-3 py-2 bg-white text-xl border shadow-sm text-indigo-900 border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-80 h-14 rounded-md sm:text-sm focus:ring-1" />
+                    <div className="text-indigo-900 cursor-pointer pt-4 pl-9 w-32 justify-center bg-indigo-200 hover:bg-amber-400 transition duration-100 ease-out hover:ease-in rounded-lg font-bold">Submit</div>
+                </div>
+
+                <div>
+                    Already using CompanyZ? <span className="cursor-pointer text-amber-400 ml-2">Sign In</span>
+                </div>
+
+            </div>
+
+            <div className="">
+                <h1>Right</h1>
+            </div>
+
+        </div>
+    )
+}
