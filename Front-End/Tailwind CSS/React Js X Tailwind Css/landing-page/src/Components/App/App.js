8907ac828b98e53fee67240b9ec591cd240d069c
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-04-30 17:39:21 +0530

    feat: :zap: Updated component hierarchy

--- Front-End/Tailwind CSS/React Js X Tailwind Css/landing-page/src/Components/App/App.js
+++ Front-End/Tailwind CSS/React Js X Tailwind Css/landing-page/src/Components/App/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="text-4xl">Hello</h1>
     </div>
   )
 }
---
+++ Front-End/Tailwind CSS/React Js X Tailwind Css/landing-page/src/Components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div>
+            <h1 className="text"></h1>
+        </div>
+    )
+}
