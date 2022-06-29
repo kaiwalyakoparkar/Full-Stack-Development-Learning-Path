ced3788a46694ca2f02b51b6152a08fe6fba34a4
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-06-29 23:58:19 +0530

    feat: Explored enums

--- Front-End/TypeScript/dist/index.js
+++ Front-End/TypeScript/dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 let arr = [1, "Kaiwalya"];
 let arr1 = [1, 2, 3];
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- Front-End/TypeScript/src/index.ts
+++ Front-End/TypeScript/src/index.ts
@@ -12,3 +12,13 @@ let arr: [number, string] = [1, "Kaiwalya"];
 
 //array
 let arr1: number[] = [1, 2, 3];
+
+//enums
+const enum size {small = 1, medium, large}
+/*
+1. small is defined as 1 so medium becomes 2, and large becomes three automatically
+
+2. adding const will generate more optimized code in the nodejs
+*/
+let mySize: size = size.medium;
+console.log(mySize);
