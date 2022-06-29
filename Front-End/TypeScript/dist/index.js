436abe9445ffdc88fd0b7b4f13043d16dedb0aa4
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-06-29 23:42:30 +0530

    feat: Explored debugging with vscode

--- Front-End/TypeScript/dist/index.js
+++ Front-End/TypeScript/dist/index.js
@@ -1,3 +1,8 @@
 "use strict";
 console.log("Hello world");
 let age = 20;
+if (age > 18) {
+    console.log("You are a major");
+    age += 11;
+}
+//# sourceMappingURL=index.js.map
--- Front-End/TypeScript/src/index.ts
+++ Front-End/TypeScript/src/index.ts
@@ -1,3 +1,8 @@
 console.log("Hello world");
 
 let age : number = 20;
+
+if (age > 18) {
+    console.log("You are a major");
+    age += 11;
+}
