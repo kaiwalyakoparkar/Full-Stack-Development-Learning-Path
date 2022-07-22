3bf9a47df7155b658e0f28a0abbc7cffd8aca327
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-07-22 17:38:36 +0530

    feat: Passed the array element to the dedicated module

---
+++ Front-End/Angular/CWH-Tutorial/todo-list/src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Front-End/Angular/CWH-Tutorial/todo-list/src/app/app.module.ts
+++ Front-End/Angular/CWH-Tutorial/todo-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
