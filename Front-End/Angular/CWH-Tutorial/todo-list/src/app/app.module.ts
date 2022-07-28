5cc902bcb71163893709ffed80dbba775d19f1a5
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-07-28 12:15:55 +0530

    feat: Add multipage support using routers

--- Front-End/Angular/CWH-Tutorial/todo-list/src/app/app.module.ts
+++ Front-End/Angular/CWH-Tutorial/todo-list/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { TodosComponent } from './MyComponents/todos/todos.component';
 import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponents/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
