4475dd85ddcc0a012566b4ccea0d145278cbd330
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-08-06 17:46:41 +0530

    feat: Local environment variable added

--- Back-End/api-practice/train-api/app.js
+++ Back-End/api-practice/train-api/app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 
 const app = express();
-const port = 8080;
 
 app.use('/', (req, res, next) => {
+    console.log("Route initiated")
     res.status(200).json({
         "messsage": "works"
     });
 })
 
-app.listen(port, ()=> {
-    console.log(`Server started on ${port}`)
-})
+module.exports = app;
